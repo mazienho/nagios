208e7cd247c6e068e59aed9083f449f52708b8c6
mazienho <mazienho@gmx.de>
2015-01-22 00:00:25 +0100

    Played around with SSH login

--- check_docker.pl
+++ check_docker.pl
@@ -25,6 +25,7 @@
 
 use strict;
 use warnings;
+use Net::SSH::Perl;
 use Nagios::Plugin;
 
 BEGIN {
@@ -35,6 +36,27 @@ BEGIN {
 }
 
 my $version = "0.0.1";
+
+my $messages = ();
+$messages->{'help_usage'}
+    = "Usage: \n ./check_snmp_uptime.pl -H <HOST> [-C <COMMUNITY>] [-o <OID>] [-w <WARNING>] [-c <CRITICAL>]";
+$messages->{'help_host'} = "IP or DNS name of the remote host";
+$messages->{'help_community'}
+    = "SNMP v1/v2c Communitystring (Default: public).";
+$messages->{'help_warning'}
+    = "Warning threshold or range (e.g. 20d:30d). Valid time units are w (week), d (day), h (hours), m (minutes), s (seconds)\n   Time addition is accepted: 20d+8h:10h+2m\nSee Nagios development guidelines for Syntax: http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT";
+$messages->{'help_critical'}
+    = "Critical threshold or range (e.g. 20d:30d). Valid time units are w (week), d (day), h (hours), m (minutes), s (seconds)\n   Time addition is accepted: 20d+8h:10h+2m\nSee Nagios development guidelines for Syntax: http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT";
+$messages->{'help_protocol'} = "SNMP Version. <1|2c|3>";
+$messages->{'help_port'}     = "Remote Port. Default: 161";
+$messages->{'help_oid'}
+    = "The numerical OID for sysuptime (e.g. \"1.3.6.1.2.1.1.3.0\"). This example is also the default value";
+$messages->{'help_timeout'} = "Timeout in seconds. Default: 30s";
+$messages->{'help_unit'}
+    = "Time unit of returned value for performance data. <d|h|s>\; Default s";
+
+#Error Messages
+$messages->{'error_threshold_syntax'} = "Wrong threshold syntax.";
 
 my $np = Nagios::Plugin->new(
     shortname => 'Docker-container',
@@ -50,9 +72,9 @@ my $np = Nagios::Plugin->new(
 $np->add_arg(
     spec     => 'host|H=s',
     help     => $messages->{'help_host'},
-    default  => "localhost"
+    default  => 'localhost',
     required => 1,
-);
+    );
 
 # Check mode
 $np->add_arg(
@@ -60,46 +82,51 @@ $np->add_arg(
     help     => $messages->{'help_mode'},
     default  => "con-count",
     required => 1,
-);
+    );
 
 # Warning threshold
 $np->add_arg(
     spec     => 'warning|w=i',
     help     => $messages->{'help_warning'},
     required => 0,
-);
+    );
 
 # Critical threshold
 $np->add_arg(
 	spec	 => 'critical|c=i',
 	help 	 => $messages->{'help_critical'},
 	required => 0,
-);
+    );
 
 # User to login
 $np->add_arg(
 	spec	 => 'user|u=s',
 	help 	 => $messages->{'help_user'},
 	required => 0,
-);
+    );
 
 # Password for user
 $np->add_arg(
 	spec	 => 'password|p=s',
 	help 	 => $messages->{'help_password'},
 	required => 0,
-);
+    );
 
 # SSH port
-np->add_arg(
+$np->add_arg(
 	spec	 => 'port|P=i',
 	help 	 => $messages->{'help_port'},
 	required => 0,
 	default	 => 22,
-);
+    );
 
-
-
+# SSH Keyfile
+$np->add_arg(
+    spec     => 'keyfile|k=s',
+    help     => $messages->{'help_keyfile'},
+    required => 0,
+    default  => "~/.ssh/id_rsa",
+    );
 
 #==============================================================#
 #                              MAIN                            #
@@ -107,6 +134,45 @@ np->add_arg(
 
 $np->getopts();
 
+my $host = $np->opts->host;
+my $user = $np->opts->user;
+my $passw = $np->opts->password;
+my $port = $np->opts->port;
+my $keyfile = $np->opts->keyfile;
+
+
+print "Options \nUser: $user\nPassword: $passw";
+
+print "Now SSH...\n";
+
+my @output = `ssh $user\@$host -p $port -i $keyfile "ls -ltr ~/"`;
+
+print "OUTPUT: \n@output";
+
+# my @output = `ssh $user\@localhost -p $port "which perl"`;
+
+# print "output: @output";
+
+
+ 
+# my $host = "bananapi";
+# my $user = "mbouws";
+# my $password = "password";
+ 
+# #-- set up a new connection
+# # my $ssh = Net::SSH::Perl->new($host);
+# # #-- authenticate
+# # $ssh->login($user, $pass);
+# #-- execute the command
+# #my($stdout, $stderr, $exit) = $ssh->cmd("ls -l /home/$user");
+# #my user = mbouws
+# #my file = /home/mbouws
+
+# my @KEYFILE = ("~/.ssh/id_rsa");
+# my $ssh = Net::SSH::Perl->new($host, debug=>1, identity_files=>\@KEYFILE);
+# #my($stdout, $stderr, $exit) = $ssh->cmd("ls -l /home/$user");
+# my @output = $ssh->cmd("ls -l /home/$user");
+# print "Output: @output";
 
 #==============================================================#
 #                          Subroutines                         #
